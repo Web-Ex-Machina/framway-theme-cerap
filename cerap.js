a1cf55cf974f63a11eef8f8c86853a96be67f03f
Quentin Vansteene <vansteene.quentin@gmail.com>
2024-07-30 18:44:24 +0200

    time to commit 2 or 3 things

--- cerap.js
+++ cerap.js
@@ -1,3 +1,67 @@
-$(function(){
+/**
+ * switch the header between reduced and not-reduced states
+ * @param  {Boolean} reduce 
+ */
+app.HeaderFW.prototype.navSwitcher = function(reduce = false){
+    var header = this;
+    if (reduce) {
+        header.$el.addClass('is-reduce');
+        if (header.$topbar) header.$topbar.appendTo(header.navPanelMenus.root.$el.find('.panel__actions'));
+        if (header.$search) header.$search.appendTo(header.navPanelMenus.root.$el.find('.panel__actions'));
+        if (header.$lang)   header.$lang.appendTo(header.navPanelMenus.root.$el.find('.panel__actions'));
+    } else {
+        header.$el.removeClass('is-reduce');
+        if (header.$topbar) header.$nav.append(header.$topbar);
+        if (header.$search) header.$nav.append(header.$search);
+        if (header.$lang)   header.$nav.append(header.$lang);
+    }
+};
 
+window.addEventListener("load", function(e) {
+    $('.sliderFW.block-card__slider').each(function(){
+        let slider = $(this);
+        slider.find('.sliderFW__arrow').each(function(){
+            $(this).appendTo(slider)
+        })
+    });
+
+
+    $('.sliderFW.cerap').each(function(){
+        let slider = $(this).sliderFW('get');
+        let contentWrapper = slider.$el.find('.sliderFW__item__content__wrapper');
+        slider.$nav.children().bind('click', () => {
+            contentWrapper.addClass('switch');
+            let delaySwitchContent = (Number(getComputedStyle(contentWrapper.get(0)).animationDuration.replace('s',''))+Number(getComputedStyle(contentWrapper.get(0)).animationDelay.replace('s','')))*1000;
+            console.log(delaySwitchContent);
+            setTimeout(()=>{
+                contentWrapper.find('.sliderFW__item__title').html(slider.$el.find('.sliderFW__item.active .sliderFW__item__hiddenContent .sliderFW__item__title').html())
+                contentWrapper.find('.sliderFW__item__subtitle').html(slider.$el.find('.sliderFW__item.active .sliderFW__item__hiddenContent .sliderFW__item__subtitle').html())
+            },delaySwitchContent/2);
+            setTimeout(()=>{
+                contentWrapper.removeClass('switch') 
+            },delaySwitchContent);
+            
+        });
+        console.log(slider);
+    })
 });
+
+
+const observerAnimate = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        // console.log(entry)
+        if (entry.target.closest('.animate--stack') == null || (entry.target.closest('.animate--stack') != null && entry.target.classList.contains('animate--stack'))) {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show')
+                if (!entry.target.classList.contains('replay'))
+                    entry.target.classList.add('lock')
+            } else {
+                if (!entry.target.classList.contains('lock'))
+                    entry.target.classList.remove('show')
+            }
+        }
+    })
+});
+
+const animateElements = document.querySelectorAll('[class*=animate--]');
+animateElements.forEach((el)=> observerAnimate.observe(el));
--- config.js
+++ config.js
@@ -1,1 +1,44 @@
-module.exports = {};
+module.exports = {
+	'colors' : {
+		'greystronger' : '#4C4E5D',
+		'grey' : '#707070',
+		'greylight' : '#bab1af',
+		'blue' : '#32399C',
+		'green' : '#1B9C8B',
+		'aquamarine': '#288AA0',
+		'mauve': '#9C2F82',
+		'brown': '#411f0f',
+	},
+
+	'primary': 'colors(mauve)',
+	'secondary': 'colors(green)',
+	'radius': '50px',
+
+	'body' : {
+		'background'    : 'colors(white)',
+		'font-color'    : 'colors(brown)',
+		'font-size'		: '16px',
+		'font-size-sm'	: '16px',
+		'font-size-xxs'	: '14px',
+	},
+	'footer': {
+		'background': 'colors(greystronger)',
+		'font-color': 'colors(white)'
+	},
+	'input': {
+		'radius': '50px',
+	},
+	'btn':{
+		'radius': '50px',
+		'font-weight': 600,
+	},
+	'container':{
+		'xl'  : '1320px', // 1140px
+		'lg'  : '1140px', // 960px
+		// 'md'  : '960px',
+		// 'sm'  : '720px',
+		// 'xs'  : '540px',
+		// 'xxs' : '100%',
+	},
+};
+
